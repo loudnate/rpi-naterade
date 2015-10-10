0f1b9cf9f17f7e4035206da05577406e4c614a68
Nathan Racklyeft <loudnate@gmail.com>
2015-10-09 19:18:14 -0700

    Updating USB device path for power-cycle script

--- root/fix-stick.sh
+++ root/fix-stick.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo "Power-cycling USB to fix dead Carelink stick"
 sleep 0.1
-echo 0 > /sys/devices/platform/bcm2708_usb/buspower
-sleep 1
-echo 1 > /sys/devices/platform/bcm2708_usb/buspower
+#echo 0 > /sys/devices/platform/bcm2708_usb/buspower
+echo 0 > /sys/devices/platform/soc/3f980000.usb/buspower
 sleep 2
+#echo 1 > /sys/devices/platform/bcm2708_usb/buspower
+echo 1 > /sys/devices/platform/soc/3f980000.usb/buspower
+sleep 3
